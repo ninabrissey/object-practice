4fc58c6471e8458fe615dcc6685ebc2c798d5069
Nina Brissey <nbrissey@gmail.com>
2021-04-02 10:37:02 -0700

    Add two variables

--- object-examples.js
+++ object-examples.js
@@ -5,3 +5,18 @@ year: 1999,
 licensePlate: 'hjk9kjs',
 maxSpeed: 150
 }
+
+var animal = {
+  name: 'dog',
+  age: 2,
+  gender: 'female',
+  species: 'mammal'
+}
+
+var customer = {
+  firstName: 'Nina',
+  lastName: 'Brissey',
+  gender: 'female',
+  age: 30,
+  shoeSize: 7.5
+}
